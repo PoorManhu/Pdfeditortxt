47fc0e4ba513efc7eb66884db92d1d3bea8bd5d9
PoorManhu <158797332+PoorManhu@users.noreply.github.com>
2024-04-05 00:36:06 +0530

    Add files via upload

---
+++ modules/vars.py
@@ -0,0 +1,7 @@
+from os import getenv
+
+
+
+API_ID = int(getenv("API_ID", 21386395))
+API_HASH = getenv("API_HASH", "d8058892a71f31cede948eb24cefdae7")
+BOT_TOKEN = getenv("BOT_TOKEN", "6964438430:AAGQ5GCdkp6CCvcCVPgdKMS1yvUq1U9IEzc")
