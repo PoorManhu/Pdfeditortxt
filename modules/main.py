6e74b2c5d11aa3ec9ce1eeecec3ecdad3e89a474
PoorManhu <158797332+PoorManhu@users.noreply.github.com>
2024-04-05 01:11:23 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -172,25 +172,26 @@ async def account_login(bot: Client, m: Message):
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
-                        copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        count+=1
+                        copy = await _.send_document(message.chat.id, document=ka, caption=cc1)
+                        count += 1
                         os.remove(ka)
                         time.sleep(1)
                     except FloodWait as e:
                         await m.reply_text(str(e))
                         time.sleep(e.x)
                         continue
+
                 
                 elif ".pdf" in url:
                     try:
-                        cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
+                        cmd = f'yt-dlp -o "{name} Bʏ : {raw_text3}.pdf" "{url}"'
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
-                        copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.pdf', caption=cc1)
+                        copy = await _.send_document(message.chat.id, document=f'{name} Bʏ : {raw_text3}.pdf', caption=cc1)
                         count += 1
-                        os.remove(f'{name}.pdf')
+                        os.remove(f'{name} Bʏ : {raw_text3}.pdf')
                     except FloodWait as e:
-                        await m.reply_text(str(e))
+                        await message.reply_text(str(e))
                         time.sleep(e.x)
                         continue
                 else:
